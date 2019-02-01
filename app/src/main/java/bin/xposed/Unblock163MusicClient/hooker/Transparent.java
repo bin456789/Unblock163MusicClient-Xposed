0bc862077b96199171d2263257db05c9a1bd6e43
bin456789 <bin456789@gmail.com>
2019-02-02 00:58:57 +0800

    Fix lyric padding

--- app/src/main/java/bin/xposed/Unblock163MusicClient/hooker/Transparent.java
+++ app/src/main/java/bin/xposed/Unblock163MusicClient/hooker/Transparent.java
@@ -1,79 +1,24 @@
 package bin.xposed.Unblock163MusicClient.hooker;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Point;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+
+import com.gyf.barlibrary.ImmersionBar;
 
 import bin.xposed.Unblock163MusicClient.CloudMusicPackage;
 import bin.xposed.Unblock163MusicClient.Hooker;
 import bin.xposed.Unblock163MusicClient.Settings;
 import de.robv.android.xposed.XC_MethodHook;
 
+import static com.gyf.barlibrary.ImmersionBar.getNavigationBarHeight;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 public class Transparent extends Hooker {
 
-    private static int getNavigationBarHeight(Context context) {
-        Resources resources = context.getResources();
-        int id = resources.getIdentifier("navigation_bar_height", "dimen", "android");
-        if (id > 0) {
-            return resources.getDimensionPixelSize(id);
-        }
-        return 0;
-    }
-
-    public static Point getNavigationBarSize(Context context) {
-        Point appUsableSize = getAppUsableScreenSize(context);
-        Point realScreenSize = getRealScreenSize(context);
-
-        // navigation bar on the side
-        if (appUsableSize.x < realScreenSize.x) {
-            return new Point(realScreenSize.x - appUsableSize.x, appUsableSize.y);
-        }
-
-        // navigation bar at the bottom
-        if (appUsableSize.y < realScreenSize.y) {
-            return new Point(appUsableSize.x, realScreenSize.y - appUsableSize.y);
-        }
-
-        // navigation bar is not present
-        return new Point();
-    }
-
-    public static Point getAppUsableScreenSize(Context context) {
-        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display = windowManager.getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        return size;
-    }
-
-    public static Point getRealScreenSize(Context context) {
-        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display = windowManager.getDefaultDisplay();
-        Point size = new Point();
-
-        if (Build.VERSION.SDK_INT >= 17) {
-            display.getRealSize(size);
-        } else {
-            try {
-                size.x = (Integer) Display.class.getMethod("getRawWidth").invoke(display);
-                size.y = (Integer) Display.class.getMethod("getRawHeight").invoke(display);
-            } catch (Exception ignored) {
-            }
-        }
-
-        return size;
-    }
 
     @Override
     protected void howToHook() throws Throwable {
@@ -86,31 +31,38 @@ public class Transparent extends Hooker {
                         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                             Activity playerActivity = (Activity) param.thisObject;
 
-                            int navigationBarHeight = getNavigationBarSize(playerActivity).y;
-                            if (navigationBarHeight > 0) {
+                            if (ImmersionBar.hasNavigationBar(playerActivity)) {
                                 ViewGroup rootView = (ViewGroup) ((ViewGroup) playerActivity.findViewById(android.R.id.content)).getChildAt(0);
-                                if (rootView instanceof RelativeLayout) {
-                                    LinearLayout linearLayout = null;
+                                LinearLayout linearLayout = null;
+                                View lyricViewContainer = null;
 
-                                    for (int i = rootView.getChildCount() - 1; i >= 0; i--) {
-                                        View child = rootView.getChildAt(i);
-                                        if (child instanceof LinearLayout) {
-                                            linearLayout = (LinearLayout) child;
-                                            break;
-                                        }
+                                for (int i = rootView.getChildCount() - 1; i >= 0; i--) {
+                                    View child = rootView.getChildAt(i);
+                                    if (linearLayout == null && child instanceof LinearLayout) {
+                                        linearLayout = (LinearLayout) child;
                                     }
+                                    if (lyricViewContainer == null && child.getClass().getName().endsWith("LyricViewContainer")) {
+                                        lyricViewContainer = child;
+                                    }
+                                }
 
-                                    if (linearLayout != null) {
-                                        linearLayout.setPadding(linearLayout.getLeft(), linearLayout.getTop(), linearLayout.getRight(),
-                                                navigationBarHeight);
-                                        playerActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-                                    }
+
+                                if (linearLayout != null && lyricViewContainer != null) {
+                                    int navigationBarHeight = getNavigationBarHeight(playerActivity);
+                                    addPaddingBottom(linearLayout, navigationBarHeight);
+                                    addPaddingBottom(lyricViewContainer, navigationBarHeight);
+                                    playerActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
                                 }
                             }
                         }
                     });
 
         }
+    }
+
+    private void addPaddingBottom(View view, int bottom) {
+        view.setPadding(view.getPaddingLeft(), view.getPaddingTop(), view.getPaddingRight(),
+                view.getPaddingBottom() + bottom);
     }
 
 }
